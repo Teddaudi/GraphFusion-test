a78441c3af0f000d6c15f841485ee8c47450a1bb
Teddaudi <daudited@gmail.com>
2024-10-31 08:59:37 +0300

    fixed hook error

--- app/nodes/page.tsx
+++ app/nodes/page.tsx
@@ -35,7 +35,7 @@ const Page = () => {
   useEffect(() => {
     fetchGraphData()
 
-  }, [])
+  })
 
   return (
     <>
--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ const GraphComponent = () => {
 
     useEffect(() => {
         fetchGraphData();
-    }, []);
+    });
 
     const drawBarGraph = (nodes: NodeData[], links: LinkData[]) => {
         if (!svgRef.current) return;
